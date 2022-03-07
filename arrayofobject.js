3618f8f22cdf5c8bd0064db2bf9245060084ebef
edoprayoga1999 <edoprayoga.se@gmail.com>
2022-03-07 13:23:15 +0700

    commit pertama

---
+++ arrayofobject.js
@@ -0,0 +1,34 @@
+const product = [
+    {
+        name: "Mouse",
+        price: 20000,
+        stock: 100,
+        remark: "Merk Razer"
+    },
+    {
+        name: "Keyboard",
+        price: 100000,
+        stock: 50,
+        remark: "Merk Corsair"
+    },
+    {
+        name: "Headset",
+        price: 150000,
+        stock: 20,
+        remark: "Merk Logitech"
+    },
+    {
+        name: "Speaker",
+        price: 300000,
+        stock: 60,
+        remark: "Merk JBL"
+    },
+    {
+        name: "Mousepad",
+        price: 15000,
+        stock: 200,
+        remark: "Tipe basic polos"
+    },
+
+]
+console.log(product[0].remark)
